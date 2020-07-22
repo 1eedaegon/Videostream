688058cacd685323a19c16d3959b659b3c1f51e2
1eedaegon <d8726243@gmail.com>
2020-07-23 00:51:07 +0900

    Initialize web service setting according to MVC

---
+++ controllers/videoController.js
@@ -0,0 +1,39 @@
+// import { videos } from "../db";
+import routes from "../routes";
+import Video from "../models/Video";
+
+export const home = async (req, res) => {
+  try {
+    const videos = await Video.find({});
+    res.render("home", { pageTitle: "Home", videos });
+  } catch (error) {
+    console.log(error);
+    res.render("home", { pageTitle: "Home", videos: [] });
+  }
+};
+export const search = (req, res) => {
+  const {
+    query: { term: searchingBy },
+  } = req;
+  res.render("search", { pageTitle: "Search", searchingBy, videos });
+};
+export const getUpload = (req, res) =>
+  res.render("upload", { pageTitle: "Upload" });
+
+export const postUpload = async (req, res) => {
+  const {
+    body: { title, description },
+    file: { path },
+  } = req;
+  const newVideo = await Video.create({ fileUrl: path, title, description });
+  console.log(title, description, path);
+  console.log(newVideo);
+  res.redirect(routes.detailVideo(newVideo.id));
+};
+
+export const detailVideo = (req, res) =>
+  res.render("detailVideo", { pageTitle: "Detail video" });
+export const editVideo = (req, res) =>
+  res.render("editVideo", { pageTitle: "Edit video" });
+// export const deleteVideo = (req, res) =>
+//   res.render("deleteVideo", { pageTitle: "Delete video" });
